5779366e54ea688f5e663d2448ca74b22b795ad3
hj he <applyhhj@163.com>
2015-12-21 14:05:54 +0800

    add splitter

--- graph/runSplitter.m
+++ graph/runSplitter.m
@@ -1,16 +1,13 @@
 clc;clear;close all;
 
-casedata='case30';
+casedata='case9241pegase';
 [F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
     TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
     ANGMIN, ANGMAX, MU_ANGMIN, MU_ANGMAX] = idx_brch;
 
-N=9;
+N=300;
 
 [baseMVA, bus, gen, branch] = loadcase(casedata);
 [i2e, bus, gen, branch] = ext2int(bus, gen, branch);
 
-bn=size(bus,1);
-[ref, pv, pq] = bustypes(bus, gen);
-C=sparse(branch(:,F_BUS),branch(:,T_BUS),1,bn,bn);
-bus = reassignZone( bus,ref,C,N );
+bus = reassignZone( bus,gen,branch,N );
